014cf237760fc5a2f8591b8c000b9b425442be27
GangDangOO <dnj456@gmail.com>
2022-04-21 23:53:58 +0900

    2022-04-21

--- 9. 재귀/10872.cpp
+++ 9. 재귀/10872.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
+int Solution(int input);
+
 int main() {
+	int input;
 	
+	cin >> input;
+	cout << Solution(input);
 	
 	return 0;
+}
+
+int Solution(int input) {
+	if (input <= 1)
+		return 1;
+	return input * Solution(input - 1);
 }
