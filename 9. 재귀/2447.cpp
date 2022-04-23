b84976f0bf7bba4fde12785187d5a58e6efae20d
GangDangOO <dnj456@gmail.com>
2022-04-23 22:33:39 +0900

    2022-04-23

---
+++ 9. 재귀/2447.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void Soltuion(int input);
+
+int main() {
+	int input;
+	
+	cin << input;
+	Solution(input);
+	
+	return 0;
+}
+
+void Soltuion(int input) {
+	
+}
