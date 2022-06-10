a32a695578be360d0761f986e24efe5649924ca8
GangDangOO <dnj456@gmail.com>
2022-06-10 18:56:11 +0900

    2022-06-10

--- 5597.cpp
+++ 5597.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
+	int input, count = 0;
+	bool arr[30];
 	
-	
+	for (int i = 0; i < 30; i++) {
+		arr[i] = false;
+	}
+	for (int i = 0; i < 28; i++) {
+		cin >> input;
+		arr[input - 1] = true;
+	}
+	for (int i = 0; i < 30; i++) {
+		if (arr[i] == false) {
+			cout << i + 1 << endl;
+			count++;
+			if (count == 2) break;
+		}
+	}
 	return 0;
 }
