935100618fd6c537e5e94417b29959b562b15b95
Nani_Owner <50565670+GangDangOO@users.noreply.github.com>
2023-04-18 17:04:03 +0900

    10813

---
+++ 4. 1차원 배열/10813.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int arr[100];
+	int n, m;
+	
+	cin >> n >> m; 
+	for(int i = 1; i <= n; i++)
+	{
+		arr[i] = i;
+	}
+	
+	for(int i = 0; i < m; i++)
+	{
+		int j, k, temp;
+		cin >> j >> k;
+		temp = arr[j];
+		arr[j] = arr[k];
+		arr[k] = temp;
+		
+	}
+	
+	for(int i = 1; i <= n; i++)
+	{
+		cout << arr[i] << ' ';
+	}
+	return 0;
+}
